7d0feb3a58a11e462735dfabdf3a1e0feb9a148b
EslemU <46886491+EslemU@users.noreply.github.com>
2019-03-18 13:39:43 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,22 @@
+<html>
+   <head>
+      <title>Accueil</title>
+	   <div align="center">
+	   
+	    <h2>Accueil</h2>
+		
+	  <link rel="stylesheet" type="text/css" href="inscription.css">
+      <meta charset="utf-8">
+   </head>
+   <body >
+   
+
+   
+  
+   <button> <a href="inscription.php" class="boutton1" > Inscription </a> </button>
+	<button> <a href="" class="boutton1" > Connexion </a> </button>
+   
+   </body>
+   
+ </html>
+ 
